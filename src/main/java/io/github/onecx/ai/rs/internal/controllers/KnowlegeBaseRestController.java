a1ec689bc8d0961b1e7a2fea5df9d6649b4e71b2
Michael Gloeckner <michael.gloeckner@capgemini.com>
2024-06-07 09:39:39 +0200

    fixed package problem

--- src/main/java/io/github/onecx/ai/rs/internal/controllers/KnowlegeBaseRestController.java
+++ src/main/java/io/github/onecx/ai/rs/internal/controllers/KnowlegeBaseRestController.java
@@ -2,7 +2,6 @@ package io.github.onecx.ai.rs.internal.controllers;
 
 import static jakarta.transaction.Transactional.TxType.NOT_SUPPORTED;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.enterprise.context.ApplicationScoped;
@@ -15,23 +14,12 @@ import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
 
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.resteasy.reactive.RestResponse;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 import org.tkit.quarkus.jpa.exceptions.ConstraintException;
 
-import gen.io.github.onecx.ai.clients.model.ChatMessage;
-import gen.io.github.onecx.ai.clients.model.ChatRequest;
-import gen.io.github.onecx.ai.clients.model.Conversation;
-import gen.io.github.onecx.ai.rs.internal.AiKnowledgeBaseInternalApi
+import gen.io.github.onecx.ai.rs.internal.AiKnowledgeBaseInternalApi;
 import gen.io.github.onecx.ai.rs.internal.model.*;
-import io.github.onecx.chat.domain.daos.ChatDAO;
-import io.github.onecx.chat.domain.daos.MessageDAO;
-import io.github.onecx.chat.domain.daos.ParticipantDAO;
-import io.github.onecx.chat.domain.models.Chat.ChatType;
-import io.github.onecx.chat.domain.models.Message;
-import io.github.onecx.chat.domain.models.Participant;
-import io.github.onecx.chat.rs.internal.mappers.ChatMapper;
 import io.github.onecx.ai.domain.daos.AIKnowledgeBaseDAO;
 import io.github.onecx.ai.rs.internal.mappers.ExceptionMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -45,172 +33,10 @@ public class KnowlegeBaseRestController implements AiKnowledgeBaseInternalApi {
     AIKnowledgeBaseDAO dao;
 
     @Inject
-    ParticipantDAO participantDao;
-
-    @Inject
-    MessageDAO msgDao;
-
-    @Inject
-    ChatMapper mapper;
-
-    @Inject
     ExceptionMapper exceptionMapper;
 
     @Context
     UriInfo uriInfo;
-
-    @Override
-    @Transactional
-    public Response createChat(CreateChatDTO createChatDTO) {
-
-        var chat = mapper.create(createChatDTO);
-        chat = dao.create(chat);
-
-        if (createChatDTO.getParticipants() != null && !createChatDTO.getParticipants().isEmpty()) {
-            var participants = mapper.mapParticipantDTOs(createChatDTO.getParticipants());
-            for (Participant participant : participants) {
-                participant.setChat(chat);
-                participant = participantDao.create(participant);
-            }
-
-        }
-
-        return Response
-                .created(uriInfo.getAbsolutePathBuilder().path(chat.getId()).build())
-                .entity(mapper.mapChat(chat))
-                .build();
-    }
-
-    @Override
-    @Transactional
-    public Response deleteChat(String id) {
-        dao.deleteQueryById(id);
-        return Response.noContent().build();
-    }
-
-    @Override
-    public Response getChatById(String id) {
-        var chat = dao.findById(id);
-        if (chat == null) {
-            return Response.status(Response.Status.NOT_FOUND).build();
-        }
-        return Response.ok(mapper.mapChat(chat)).build();
-    }
-
-    @Override
-    public Response getChats(Integer pageNumber, Integer pageSize) {
-        var items = dao.findAll(pageNumber, pageSize);
-        return Response.ok(mapper.mapPage(items)).build();
-    }
-
-    @Override
-    public Response searchChats(ChatSearchCriteriaDTO chatSearchCriteriaDTO) {
-        var criteria = mapper.map(chatSearchCriteriaDTO);
-        var result = dao.findChatsByCriteria(criteria);
-        return Response.ok(mapper.mapPage(result)).build();
-    }
-
-    @Override
-    @Transactional
-    public Response updateChat(String id, UpdateChatDTO updateChatDTO) {
-
-        var chat = dao.findById(id);
-        if (chat == null) {
-            return Response.status(Response.Status.NOT_FOUND).build();
-        }
-
-        mapper.update(updateChatDTO, chat);
-        dao.update(chat);
-        return Response.noContent().build();
-    }
-
-    @Override
-    @Transactional
-    public Response createChatMessage(String chatId, CreateMessageDTO createMessageDTO) {
-
-        var chat = dao.findById(chatId);
-
-        if (chat == null) {
-            throw new ConstraintException("Chat does not exist", ChatErrorKeys.CHAT_DOES_NOT_EXIST, null);
-        }
-
-        var message = mapper.createMessage(createMessageDTO);
-        message.setChat(chat);
-        message = msgDao.create(message);
-
-        if (ChatType.AI_CHAT.equals(chat.getType())) {
-
-            Conversation conversation = mapper.mapChat2Conversation(chat);
-            ChatMessage chatMessage = mapper.mapMessage(message);
-
-            ChatRequest chatRequest = new ChatRequest();
-            chatRequest.chatMessage(chatMessage);
-            chatRequest.conversation(conversation);
-            Response response = aiChatClient.chat(chatRequest);
-
-            var chatResponse = response.readEntity(ChatMessage.class);
-
-            var responseMessage = mapper.mapAiSvcMessage(chatResponse);
-            responseMessage.setChat(chat);
-            msgDao.create(responseMessage);
-
-        }
-
-        return Response
-                .created(uriInfo.getAbsolutePathBuilder().path(message.getId()).build())
-                .build();
-
-    }
-
-    @Override
-    public Response getChatMessages(String chatId) {
-        var chat = dao.findById(chatId);
-
-        if (chat == null || chat.getMessages() == null) {
-            // Handle the case where chat or its messages are null
-            return Response.status(Response.Status.NOT_FOUND).build();
-        }
-
-        var messages = chat.getMessages();
-        List<Message> messageList = new ArrayList<>(messages);
-        return Response.ok(mapper.mapMessageList(messageList)).build();
-
-    }
-
-    @Override
-    public Response addParticipant(String chatId, @Valid @NotNull AddParticipantDTO addParticipantDTO) {
-
-        var chat = dao.findById(chatId);
-
-        if (chat == null) {
-            throw new ConstraintException("Chat does not exist", ChatErrorKeys.CHAT_DOES_NOT_EXIST, null);
-        }
-
-        var participant = mapper.addParticipant(addParticipantDTO);
-        participant.setChat(chat);
-        participant = participantDao.create(participant);
-
-        return Response
-                .created(uriInfo.getAbsolutePathBuilder().path(participant.getId()).build())
-                .build();
-
-    }
-
-    @Override
-    public Response getChatParticipants(String chatId) {
-
-        var chat = dao.findById(chatId);
-
-        if (chat == null || chat.getParticipants() == null) {
-            // Handle the case where chat or its messages are null
-            return Response.status(Response.Status.NOT_FOUND).build();
-        }
-
-        var participants = chat.getParticipants();
-        List<Participant> participantList = new ArrayList<>(participants);
-        return Response.ok(mapper.mapParticipantList(participantList)).build();
-
-    }
 
     @ServerExceptionMapper
     public RestResponse<ProblemDetailResponseDTO> exception(ConstraintException ex) {
@@ -404,6 +230,5 @@ public class KnowlegeBaseRestController implements AiKnowledgeBaseInternalApi {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'uploadKnowledgeDocuments'");
     }
-
 
 }
---
+++ src/main/java/io/github/onecx/ai/rs/internal/mappers/ExceptionMapper.java
@@ -0,0 +1,69 @@
+package io.github.onecx.ai.rs.internal.mappers;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Path;
+import jakarta.ws.rs.core.Response;
+
+import org.jboss.resteasy.reactive.RestResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.tkit.quarkus.jpa.exceptions.ConstraintException;
+import org.tkit.quarkus.rs.mappers.OffsetDateTimeMapper;
+
+import gen.io.github.onecx.ai.rs.internal.model.ProblemDetailInvalidParamDTO;
+import gen.io.github.onecx.ai.rs.internal.model.ProblemDetailParamDTO;
+import gen.io.github.onecx.ai.rs.internal.model.ProblemDetailResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Mapper(uses = { OffsetDateTimeMapper.class })
+public abstract class ExceptionMapper {
+
+    public RestResponse<ProblemDetailResponseDTO> constraint(ConstraintViolationException ex) {
+        var dto = exception("CONSTRAINT_VIOLATIONS", ex.getMessage());
+        dto.setInvalidParams(createErrorValidationResponse(ex.getConstraintViolations()));
+        return RestResponse.status(Response.Status.BAD_REQUEST, dto);
+    }
+
+    public RestResponse<ProblemDetailResponseDTO> exception(ConstraintException ex) {
+        var dto = exception(ex.getMessageKey().name(), ex.getConstraints());
+        dto.setParams(map(ex.namedParameters));
+        return RestResponse.status(Response.Status.BAD_REQUEST, dto);
+    }
+
+    @Mapping(target = "removeParamsItem", ignore = true)
+    @Mapping(target = "params", ignore = true)
+    @Mapping(target = "invalidParams", ignore = true)
+    @Mapping(target = "removeInvalidParamsItem", ignore = true)
+    public abstract ProblemDetailResponseDTO exception(String errorCode, String detail);
+
+    public List<ProblemDetailParamDTO> map(Map<String, Object> params) {
+        if (params == null) {
+            return List.of();
+        }
+        return params.entrySet().stream().map(e -> {
+            var item = new ProblemDetailParamDTO();
+            item.setKey(e.getKey());
+            if (e.getValue() != null) {
+                item.setValue(e.getValue().toString());
+            }
+            return item;
+        }).toList();
+    }
+
+    public abstract List<ProblemDetailInvalidParamDTO> createErrorValidationResponse(
+            Set<ConstraintViolation<?>> constraintViolation);
+
+    @Mapping(target = "name", source = "propertyPath")
+    @Mapping(target = "message", source = "message")
+    public abstract ProblemDetailInvalidParamDTO createError(ConstraintViolation<?> constraintViolation);
+
+    public String mapPath(Path path) {
+        return path.toString();
+    }
+}
